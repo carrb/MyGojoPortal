145f5370a55ffd53d349fcf40cc7019af103dc3b
Scott Wade <wades@gojo.com>
2012-07-07 18:31:12 -0400

    Server.js is working file system storage.

--- src/SiteLinks/SiteLinks.Service/server.js
+++ src/SiteLinks/SiteLinks.Service/server.js
@@ -1,83 +1,63 @@
 
-/*
-var Currency = require('currency');
-var conversionRateCanadian = 0.91;
-
-Currency = new Currency(conversionRateCanadian);
-
-console.log('Currency class instantiated with current conv. rate.');
-
-console.log("50 Canadian dollars: $" + Currency.canadianToUS(50));
-console.log("30 US dollars: Can$" + Currency.USToCanadian(30));
-*/
+var fs = require('fs'),
+    path = require('path'),
+    args = process.argv.splice(2),
+    command = args.shift(),
+    taskDescription = args.join(' '),
+    file = path.join(process.cwd(), '/.tasks');
 
 
+switch(command) {
+    case 'list':
+        listTasks(file);
+        break;
 
-var http = require('http');
-var Url = require('url');
+    case 'add':
+        addTask(file, taskDescription);
+        break;
 
-var items = [];
-console.log('items array is empty; creating httpServer on 8080...');
+    default:
+        console.log('Usage: ' + process.argv[0] + ' list|add [taskDescription]');
+         
+}
 
 
-http.createServer(function(req, res) {
-    switch(req.method) {
-        case 'POST':
-            var item = '';
-            req.setEncoding('utf8');
+function getTasks(file, cb) {
+    path.exists(file, function(exists) {
+        var tasks = [];
 
-            req.on('data', function(chunk) {
-                item += chunk;
+        if(exists) {
+            fs.readFile(file, 'utf8', function(err, datum) {
+                if(err) throw err;
+                var data = datum.toString();
+                var tasks = JSON.parse(data);
+                cb(tasks);
             });
+        } else {
+            cb([]);
+        }
+    });
+}
 
-            req.on('end', function() {
-                items.push(item);
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end('OK\n');
-            });
-
-            break;
-
-
-        case 'GET':
-            var body = items.map(function(item, i) {
-                return i + ': ' + item;
-            }).join('\n');
-
-            res.writeHead(200, {
-                'Content-Length': Buffer.byteLength(body),
-                'Content-Type': 'text/plain; charset="utf-8"'
-            });
-
-            res.end(body);
-            break;
+function listTasks(file) {
+    getTasks(file, function(tasks) {
+        for(var i in tasks) {
+            console.log(tasks[i]);
+        }
+    });
+}
 
 
-        case 'DELETE':
-            var path = Url.parse(req.url).pathname;
-            var i = parseInt(path.slice(1), 10);
+function storeTasks(file, tasks) {
+    fs.writeFile(file, JSON.stringify(tasks), 'utf8', function(err) {
+        if (err) throw err;
+        console.log('Saved.');
+    });
+}
 
-            if(isNaN(i)) {
-                res.statusCode = 400;
-                res.end('Invalid item id');
-            } else if(!items[i]) {
-                res.statusCode = 404;
-                res.end('Item at id: ' + i + 'not found');
-            } else {
-                items.splice(i, 1);
-                res.end('OK\n');
-            }
-
-            break;
-
-    }
-
-}).listen(8080);
-
-/*
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-}).listen(process.env.PORT || 8080);
-
-*/
+function addTask(file, taskDescription) {
+    getTasks(file, function(tasks) {
+        tasks.push(taskDescription);
+        storeTasks(file, tasks);
+    });
+}
