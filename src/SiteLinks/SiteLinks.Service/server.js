ff445630847471d550d2db7eec49a9147fa9ca5c
Scott Wade <wades@gojo.com>
2012-07-11 10:19:00 -0400

    Updated MVC 4 nightlies; added Node module optimist to SiteLinks

--- src/SiteLinks/SiteLinks.Service/server.js
+++ src/SiteLinks/SiteLinks.Service/server.js
@@ -1,27 +1,67 @@
 
-var fs = require('fs'),
-    path = require('path'),
-    args = process.argv.splice(2),
-    command = args.shift(),
-    taskDescription = args.join(' '),
-    file = path.join(process.cwd(), '/.tasks');
+var argv = require('optimist').argv;
+var mongoose = require('mongoose');
+
+
+var command = argv.command;
+var taskProject = argv.project;
+var taskDescription = argv.desc;
+
+
+var db = mongoose.connect('mongodb://localhost/tasks');
+
+var Schema = mongoose.Schema;
+var Tasks = new Schema({
+    project: String,
+    description: String
+});
+
+var tasksModel = mongoose.model('Task', Tasks);
+
 
 
 switch(command) {
     case 'list':
-        listTasks(file);
+        listTasks();
         break;
 
     case 'add':
-        addTask(file, taskDescription);
+        addTask(taskProject, taskDescription);
         break;
 
     default:
-        console.log('Usage: ' + process.argv[0] + ' list|add [taskDescription]');
-         
+        console.log('Usage: ' + process.argv[0] + '--command list|add --project [taskProject] --desc [taskDescription]');
 }
 
 
+function addTask(taskProject, taskDescription) {
+    var task = new tasksModel();
+    task.project = taskProject;
+    task.description = taskDescription;
+
+    task.save(function(err) {
+        if(err) throw err;
+        console.log('Task saved!');
+    });
+}
+
+function listTasks() {
+    tasksModel.find({}, function(err, docs) {
+        if(err) throw err;
+
+        docs.forEach(function(doc) {
+            //console.log(typeof doc + ": " + doc + "\n");
+        });
+
+        //docs.forEach
+
+
+    });
+}
+
+
+
+/*
 function getTasks(file, cb) {
     path.exists(file, function(exists) {
         var tasks = [];
@@ -55,9 +95,6 @@ function storeTasks(file, tasks) {
     });
 }
 
-function addTask(file, taskDescription) {
-    getTasks(file, function(tasks) {
-        tasks.push(taskDescription);
-        storeTasks(file, tasks);
-    });
-}
+
+
+*/
